464ab463b7ac65cc89aee584128f8673d07b7426
bisslee <bisslee@gmail.com>
2025-07-13 01:19:05 -0300

    Ajuste no Migrations para não dar erro no update

--- back/ManualMovementsManager/ManualMovementsManager/src/ManualMovementsManager.CrossCutting/DependencyInjection/RepositoryServiceCollectionExtension.cs
+++ back/ManualMovementsManager/ManualMovementsManager/src/ManualMovementsManager.CrossCutting/DependencyInjection/RepositoryServiceCollectionExtension.cs
@@ -9,9 +9,26 @@ namespace ManualMovementsManager.CrossCutting.DependencyInjection
     {
         public static IServiceCollection AddRepository(this IServiceCollection services)
         {
+            // Customer repositories
             services.AddScoped<IReadRepository<Customer>, ReadRepository<Customer>>();
             services.AddScoped<IWriteRepository<Customer>, WriteRepository<Customer>>();
             services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+
+            // Product repositories
+            services.AddScoped<IReadRepository<Product>, ReadRepository<Product>>();
+            services.AddScoped<IWriteRepository<Product>, WriteRepository<Product>>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+
+            // ProductCosif repositories
+            services.AddScoped<IReadRepository<ProductCosif>, ReadRepository<ProductCosif>>();
+            services.AddScoped<IWriteRepository<ProductCosif>, WriteRepository<ProductCosif>>();
+            services.AddScoped<IProductCosifReadRepository, ProductCosifReadRepository>();
+
+            // ManualMovement repositories
+            services.AddScoped<IReadRepository<ManualMovement>, ReadRepository<ManualMovement>>();
+            services.AddScoped<IWriteRepository<ManualMovement>, WriteRepository<ManualMovement>>();
+            services.AddScoped<IManualMovementReadRepository, ManualMovementReadRepository>();
+
             return services;
         }
     }
